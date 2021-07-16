a8ecb4fa932966f008fd542fcee4e9ffb24c061a
Jonghyun John Yoo <jonghyun.yoo0323@gmail.com>
2021-07-17 01:58:55 +0900

    prod env for 07/17

--- src/main/java/org/thesix/reply/controller/ReplyController.java
+++ src/main/java/org/thesix/reply/controller/ReplyController.java
@@ -10,6 +10,9 @@ import org.thesix.reply.dto.RepliesResponseDTO;
 import org.thesix.reply.dto.RepliesUpdateRequestDTO;
 import org.thesix.reply.service.RepliesService;
 
+import static org.thesix.reply.common.util.ApiUtil.ApiResult;
+import static org.thesix.reply.common.util.ApiUtil.success;
+
 import java.util.Map;
 
 @RestController
@@ -21,40 +24,42 @@ public class ReplyController {
     private final RepliesService replyService;
 
     @GetMapping("/list/{bno}/{page}")
-    public ResponseEntity<ListResponseRepliesDTO> getList(@PathVariable Long bno, @PathVariable int page){
+    public ApiResult<ListResponseRepliesDTO> getList(@PathVariable Long bno, @PathVariable int page){
         /**
          * 해당 개시글의 댓글 목록을 30개까지 페이징 처리해서 보내주는 컨트롤러
+         replyService.getList(bno, page)
          */
-        return ResponseEntity.ok(replyService.getList(bno, page));
+
+        return success(replyService.getList(bno, page));
     }
 
     @PostMapping("/save")
-    public ResponseEntity<RepliesResponseDTO> saveReply(@RequestBody RepliesSaveRequestDTO dto){
+    public ApiResult<RepliesResponseDTO> saveReply(@RequestBody RepliesSaveRequestDTO dto){
         /**
          * 댓글 등록 컨트롤러.
          * 대댓글도 이 컨트롤러를 통해서 저장한다.
          *
          */
-        return ResponseEntity.ok(replyService.saveReply(dto));
+        return success(replyService.saveReply(dto));
     }
 
     @PutMapping("/update")
-    public ResponseEntity<RepliesResponseDTO> updateReply(@RequestBody RepliesUpdateRequestDTO dto){
+    public ApiResult<RepliesResponseDTO> updateReply(@RequestBody RepliesUpdateRequestDTO dto){
         /**
          * 댓글 수정 컨트롤러.
          * 대댓글도 이 컨트롤러를 통해서 수정한다.
          *
          */
-        return ResponseEntity.ok(replyService.updateReply(dto));
+        return success(replyService.updateReply(dto));
     }
 
     @DeleteMapping("/delete/{rno}")
-    public ResponseEntity<Map<String,String>> updateReply(@PathVariable Long rno){
+    public ApiResult<Map<String,String>> updateReply(@PathVariable Long rno){
         /**
          * 댓글 삭제 컨트롤러.
          * 대댓글도 이 컨트롤러를 통해서 삭제 처리한다.
          *
          */
-        return ResponseEntity.ok(replyService.deleteReply(rno));
+        return success(replyService.deleteReply(rno));
     }
 }
